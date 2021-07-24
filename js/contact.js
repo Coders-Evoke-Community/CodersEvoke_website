263c15621d0a1b581c7362da128b67006f0da263
Asel <peirisasel373@Gmail.com>
2021-07-25 00:50:21 +0530

    minor fix

--- js/contact.js
+++ js/contact.js
@@ -27,18 +27,18 @@ window.onload = function () {
 
 
 
-    // // generate random contact number
-    // this.contact_number.value = Math.random() * 100000 | 0;
+    // generate random contact number
+    this.contact_number.value = Math.random() * 100000 | 0;
 
-    // // // send form to mail
-    // emailjs.sendForm('contact_service', 'contact_form', this)
-    //   .then(function () {
-    //     document.getElementById('status').innerHTML = "Submitted Successfully..";
-    //     document.getElementById('contact-form').reset();
-    //   }, function (error) {
-    //     console.log('FAILED', error);
-    //     document.getElementById('status').innerHTML = "Submission Failed! Try Again.";
-    //     document.getElementById('contact-form').reset();
-    //   });
+    // send form to mail
+    emailjs.sendForm('contact_service', 'contact_form', this)
+      .then(function () {
+        document.getElementById('status').innerHTML = "Submitted Successfully..";
+        document.getElementById('contact-form').reset();
+      }, function (error) {
+        console.log('FAILED', error);
+        document.getElementById('status').innerHTML = "Submission Failed! Try Again.";
+        document.getElementById('contact-form').reset();
+      });
   });
 }
