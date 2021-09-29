fa7d5eb48eeef1bae50769e055b8c57916b83884
Ankita Sahu <sahuankitaofc.1@gmail.com>
2021-09-29 10:06:31 +0530

    about us page added

---
+++ about.js
@@ -0,0 +1,9 @@
+const navSlide=()=>{
+    const burger =document.querySelector('.burger');
+    const nav=document.querySelector('.nav-links');
+
+    burger.addEventListener('click',()=>{
+        nav.classList.toggle('nav-active');
+    });
+}
+navSlide();
